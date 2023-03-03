51e3b78bf695c8f0a306f8ce58be588c968c5068
kanykei063129 <akjoltoikyzykanykei@gmail.com>
2023-03-03 16:50:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws MyException {
+        while (true) {
+            System.out.println("Which one do you want to work Parallelepiped or Cylinder?");
+            Scanner scanner = new Scanner(System.in);
+            String a = scanner.nextLine();
+            switch (a) {
+                case "Parallelepiped":
+                    try {
+                        float height = Float.parseFloat(scanner.nextLine());
+                        float length = Float.parseFloat(scanner.nextLine());
+                        float width = Float.parseFloat(scanner.nextLine());
+                        Parallelepiped p = new Parallelepiped(height, length, width);
+                        System.out.println(p);
+                        System.out.println("Area of parallelepiped: " + p.area());
+                        System.out.println("Volume of parallelepiped: " + p.volume());
+
+                    } catch (NumberFormatException e) {
+                        throw new MyException();
+                    }
+                    break;
+                case "Cylinder":
+                    try {
+                        float radius = Float.parseFloat(scanner.nextLine());
+                        float heighte = Float.parseFloat(scanner.nextLine());
+                        float PI = Float.parseFloat(scanner.nextLine());
+                        Cylinder c = new Cylinder(radius, heighte, PI);
+                        System.out.println(c);
+                        System.out.println("myMethod of Cylinder " + c.myMethod());
+                        System.out.println("myMethod of Cylinder " + c.myMethod1());
+
+                    } catch (ArithmeticException e) {
+                        throw new MyException();
+                    }break;
+            }
+        }
+    }
+}
